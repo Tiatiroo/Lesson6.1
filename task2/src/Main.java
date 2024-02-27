5590d718126b1c72f68ce8fffaf7ee61ac46d97a
Tiatiroo <vaoborozhniy@stud.etu.ru>
2024-02-27 21:42:48 +0300

    Ver. 2

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -16,43 +16,15 @@ public class Main {
         double greatest = 0;
         double average = 0;
         double smallest = 0;
-        //todo дописать логику программы ниже.
-        if ((first > second) && (first > third)) {
-            greatest = first;
-            if (second > third) {
-                average = second;
-                smallest = third;
-            }
-            else if (third > second) {
-                average = third;
-                smallest = second;
-            }
-        }
 
-        else if ((second > first) && (second > third)) {
-            greatest = second;
-            if (first > third) {
-                average = first;
-                smallest = third;
-            }
-            else if (third > first) {
-                average = third;
-                smallest = first;
-            }
-        }
-        else if ((third > first) && (third > second)) {
-            greatest = third;
-            if (first > second) {
-                average = first;
-                smallest = second;
-            }
-            else if (second > first) {
-                average = second;
-                smallest = first;
-            }
-        }
+        greatest = (first > second) && (first > third) ? first : (second > first) && (second > third) ? second : (third > first) && (third > second) ? third : 0;
+        average = ((first < second) && (first > third)) || ((first > second) && (first < third)) ? first
+                : ((second < first) && (second > third)) || ((second > first) && (second < third)) ? second
+                : ((third < first) && (third > second)) || ((third > first) && (third < second)) ? third : 0;
+        smallest = (first < second) && (first < third) ? first : (second < first) && (second < third) ? second : (third < first) && (third < second) ? third : 0;
 
-            //todo
+
+
             System.out.println("Наибольший вес: " + greatest);
             System.out.println("Средний вес: " + average);
             System.out.println("Наименьший вес: " + smallest);
